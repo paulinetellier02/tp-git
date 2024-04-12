64653b322cedf120a0dd5661d80653fdbbd2155a
pauli <pauline.tellier@esme.fr>
2024-04-12 15:59:27 +0200

    test des fonctions

--- test_calculator.py
+++ test_calculator.py
@@ -2,3 +2,13 @@ from calculator import addition
 
 def test_addition():
     assert addition(1, 2) == 3
+
+from calculator import soustraction
+
+def test_soustraction():
+    assert soustraction(10,8) == 2
+
+from calculator import division
+
+def test_division():
+    assert division(8,2)==4
